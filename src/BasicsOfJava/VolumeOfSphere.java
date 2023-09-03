2baa77e619a279213c96ad98ebf554757ceca082
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-09-03 17:47:34 +0530

    volume of Sphere program file Added

---
+++ src/BasicsOfJava/VolumeOfSphere.java
@@ -0,0 +1,13 @@
+package BasicsOfJava;
+
+public class VolumeOfSphere {
+    public static void main(String[] args) {
+        double pie = 3.14;
+        double radius = 48;
+
+//        Volume of sphere Formula = (4/3)*pie*r3
+
+        double volume = (4.0/3.0) * pie * radius * radius * radius;
+        System.out.println(volume);
+    }
+}
