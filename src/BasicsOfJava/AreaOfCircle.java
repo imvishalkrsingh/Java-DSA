e09dd858092a04825fcb5dd2de993b4c972d343e
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-09-03 17:42:04 +0530

    Area of Circle program file added

---
+++ src/BasicsOfJava/AreaOfCircle.java
@@ -0,0 +1,10 @@
+package BasicsOfJava;
+
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        double pie = 3.14;
+        double radius = 5.0;
+        double areaOfCircle = pie * radius * radius;
+        System.out.println(areaOfCircle);
+    }
+}
