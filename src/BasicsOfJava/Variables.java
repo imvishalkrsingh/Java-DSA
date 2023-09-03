f96518ef8522c60a9c08bb3d0e4c8b4947e37960
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-09-03 15:04:03 +0530

    Variables Files added

---
+++ src/BasicsOfJava/Variables.java
@@ -0,0 +1,43 @@
+package BasicsOfJava;
+
+public class Variables {
+    public static void main(String[] args) {
+//        declaration
+        int x;
+//        initialization
+        x = 5;
+        System.out.println(x);
+
+//        Re-declaration
+        x=400;
+        System.out.println(x);
+
+//        mathematical calculation with variables
+        System.out.println(x+9);
+
+        x = x-3;
+        System.out.println(x);
+
+//        printing variables and update variables
+
+        int y = 100;
+        System.out.print("Value of y : ");
+        System.out.println(y);
+
+//        Re-Declaration
+        y = 200;
+        System.out.print("After Redeclaration of y : ");
+        System.out.println(y);
+
+//        update value of y
+        y = y+50;
+        System.out.print("After update : ");
+        System.out.println(y);
+
+
+
+
+
+
+    }
+}
