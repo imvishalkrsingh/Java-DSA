f97edcf6485402af11d56d9f95d6e6b0a27f98b6
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-09-03 15:23:05 +0530

    Arithmetic_Operation files added

---
+++ src/BasicsOfJava/Arithmetic_Operation.java
@@ -0,0 +1,29 @@
+package BasicsOfJava;
+
+public class Arithmetic_Operation {
+    public static void main(String[] args) {
+
+        int x = 5;
+        int y = 10;
+
+//        addition of two numbers (x and Y)
+        System.out.print("Addition of x and y : ");
+        System.out.println(x+y);
+
+//        Subtraction of two numbers (x and Y)
+        System.out.print("Subtraction of x and y : ");
+        System.out.println(x-y);
+
+//        Multiplication of two numbers (x and y)
+        System.out.print("Multiplication of x and y : ");
+        System.out.println(x * y);
+
+//        Division of two numbers ( x and y)
+        System.out.print("Division of x and y : ");
+        System.out.println(x/y);
+
+//        mod of two numbers
+        System.out.print("Mod of x and y : ");
+        System.out.println(x%y);
+    }
+}
