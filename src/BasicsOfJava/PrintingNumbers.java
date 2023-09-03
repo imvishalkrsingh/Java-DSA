fd69c14f0989c251b39cf5a4b7b0278c2e22be30
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-09-03 14:34:42 +0530

    print numbers

---
+++ src/BasicsOfJava/PrintingNumbers.java
@@ -0,0 +1,20 @@
+package BasicsOfJava;
+
+public class PrintingNumbers {
+    public static void main(String[] args) {
+//        print strings
+        System.out.println("Vishal kr Singh");
+//        print integers
+        System.out.println(4);
+//        print addition
+        System.out.println(4+3);
+
+//        print numbers in double quote
+        System.out.println("444");
+//        print addition in double quote
+        System.out.println("3+5");
+
+//       You can not print string without double quote
+        System.out.println("Hey");
+    }
+}
