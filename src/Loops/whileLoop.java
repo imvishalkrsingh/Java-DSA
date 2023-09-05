bc2e2b2bd78c41d76d9453ed8c1c776a856ba010
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-09-05 18:07:49 +0530

    first whileLoop program file added

---
+++ src/Loops/whileLoop.java
@@ -0,0 +1,18 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class whileLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the value of N : ");
+        int n = sc.nextInt();
+
+        int num = 1;
+        while (num <= n){
+            System.out.println(num);
+            num++;
+        }
+    }
+}
