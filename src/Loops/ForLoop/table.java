669757c7b57cfb40ccde040a89ef4b6d2275d405
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-09-07 09:42:31 +0530

    factorial and table program file added

---
+++ src/Loops/ForLoop/table.java
@@ -0,0 +1,15 @@
+package Loops.ForLoop;
+import java.util.Scanner;
+public class table {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the Table Number : ");
+        int n = sc.nextInt();
+
+        for(int i = 1; i<=10; i++){
+          int result = n * i;
+            System.out.println(n + " * " + i + " = "  + result );
+        }
+    }
+}
