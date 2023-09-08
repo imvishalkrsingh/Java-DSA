a6cb0940ae3b1b3f6c87b51b638fb9f35f9f6818
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-09-08 15:33:53 +0530

    fibonacci series program file added

---
+++ src/Loops/ForLoop/fibonacci_series.java
@@ -0,0 +1,27 @@
+package Loops.ForLoop;
+import java.util.Scanner;
+
+public class fibonacci_series {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int number;  // To hold number of terms
+
+        int firstTerm = 0,
+                secondTerm = 1,
+                thirdTerm;
+
+        System.out.print("Enter number of terms of series : ");
+        number = sc.nextInt();
+
+        System.out.print(firstTerm + " " + secondTerm + " ");
+
+        for(int i = 3; i <= number; i++)
+        {
+            thirdTerm = firstTerm + secondTerm;
+            System.out.print(thirdTerm + " ");
+            firstTerm = secondTerm;
+            secondTerm = thirdTerm;
+        }
+    }
+}
