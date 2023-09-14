d2fa1106f25625565a76c085d3e6addf9665bcd0
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-09-14 19:22:05 +0530

    preDefinedMethod program file added

---
+++ src/Oops/Basics/preDefinedMethods.java
@@ -0,0 +1,21 @@
+package Oops.Basics;
+
+public class preDefinedMethods {
+    public static void main(String[] args) {
+
+        System.out.print("Ceil Value of 6.9 : ");
+        System.out.println(Math.ceil(6.9));
+
+        System.out.print("Floor Value of 5.3 : ");
+        System.out.println(Math.floor(5.3));
+
+        System.out.print("Square Root of 24 : ");
+        System.out.println(Math.sqrt(24));
+
+        System.out.print("Square Root of 16 : ");
+        System.out.println(Math.sqrt(16));
+
+
+
+    }
+}
