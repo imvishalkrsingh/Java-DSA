968aaf3af80427bc5537dd22946dc3efc9fd4ce1
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-09-15 17:34:04 +0530

    passByValue concept exam file added

---
+++ src/Oops/Basics/passByValue.java
@@ -0,0 +1,21 @@
+package Oops.Basics;
+public class passByValue {
+
+    public static void decrease(int n1, int n2){
+        n1--;
+        n2 = n2-2;
+        System.out.print("Value of Formal parameters");
+        System.out.println(n1 + " : " + n2);
+        // n1 and n2 are formal parameters
+    }
+    public static void main(String[] args) {
+
+        int p = 26;
+        int q = 13;
+
+        decrease(p,q);
+        System.out.print("Value of Actual parameters");
+        System.out.println(p + " : " + q);
+
+    }
+}
