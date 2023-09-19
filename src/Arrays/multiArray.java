48b04480cdeaa155c35ccd81f1924ba0d2205e7f
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-09-19 10:35:41 +0530

    multiArray program file added

---
+++ src/Arrays/multiArray.java
@@ -0,0 +1,52 @@
+package Arrays;
+
+public class multiArray {
+    public static void main(String[] args) {
+        int[][] arr = new int[3][3];
+        System.out.println("Length of arr : " + arr.length);
+
+        System.out.println("Value of arr : ");
+        System.out.print(arr[0][0] +" ");
+        System.out.print(arr[0][1] +" ");
+        System.out.print(arr[0][2] +" ");
+
+        System.out.println();
+
+        System.out.print(arr[1][0] +" ");
+        System.out.print(arr[1][1] +" ");
+        System.out.print(arr[1][2] +" ");
+
+        System.out.println();
+
+        System.out.print(arr[2][0] +" ");
+        System.out.print(arr[2][1] +" ");
+        System.out.print(arr[2][2] +" ");
+
+        System.out.println();
+        System.out.println();
+
+
+        int[][] arr_1 = { {10,11,12},{13,14,15},{16,17,18} };
+        System.out.println("Length of arr_1 : " + arr_1.length);
+
+        System.out.println("Value of arr_1 : ");
+        System.out.print(arr_1[0][0] +" ");
+        System.out.print(arr_1[0][1] +" ");
+        System.out.print(arr_1[0][2] +" ");
+
+        System.out.println();
+
+        System.out.print(arr_1[1][0] +" ");
+        System.out.print(arr_1[1][1] +" ");
+        System.out.print(arr_1[1][2] +" ");
+
+        System.out.println();
+
+        System.out.print(arr_1[2][0] +" ");
+        System.out.print(arr_1[2][1] +" ");
+        System.out.print(arr_1[2][2] +" ");
+
+        System.out.println();
+
+    }
+}
