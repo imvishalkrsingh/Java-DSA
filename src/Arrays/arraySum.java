1763c41237657931f9c6108a453bd7feec600f37
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-09-19 11:18:15 +0530

    arraySum program file added

---
+++ src/Arrays/arraySum.java
@@ -0,0 +1,15 @@
+package Arrays;
+
+public class arraySum {
+    public static void main(String[] args) {
+
+        int[] arr = {12,13,14};
+
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++){
+            sum = sum + arr[i];
+        }
+        System.out.print(sum);
+    }
+}
