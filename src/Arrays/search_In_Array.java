94b1d7466923212e5ac30196b4a8acaaf12fadac
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-09-19 16:13:57 +0530

    search_In_Array program file added

---
+++ src/Arrays/search_In_Array.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class search_In_Array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] number = {2, 3, 4, 9, 7, 8, 1, 0};
+
+        System.out.print("Array is : ");
+        for (int i=0; i<number.length; i++){
+            System.out.print(number[i] + " ");
+        }
+        System.out.println();
+        System.out.print("Enter number which you want to search from the array List : ");
+        int search = sc.nextInt();
+        int ans = -1;
+
+        for (int i = 0; i < number.length; i++) {
+            if (number[i] == search) {
+                ans = i;
+                System.out.println( "Found " + search + " at index "  + ans );
+            }
+        }
+    }
+}
