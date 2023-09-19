224d972414ced456ab501b6f9b05626f462d57a5
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-09-19 14:11:14 +0530

    maxOfArray program file added

---
+++ src/Arrays/maxOfArray.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+public class maxOfArray {
+    public static void main(String[] args) {
+        int[] number = {2,3,4,9,7,8,1,0};
+        int max = 0;
+
+        for (int i = 0; i < number.length; i++){
+            if(number[i] > max){
+                max = number[i];
+            }
+        }
+
+        System.out.println("Maximum Number in this array : " + max);
+    }
+}
