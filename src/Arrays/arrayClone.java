93bf59401df938a420d303b000de57389e56d770
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-09-20 22:45:45 +0530

    arrayClone program file added

---
+++ src/Arrays/arrayClone.java
@@ -0,0 +1,37 @@
+package Arrays;
+
+import java.util.Scanner;
+public class arrayClone {
+
+    static void printArray(int[] arr){
+
+        for (int i = 0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array : ");
+        int size = sc.nextInt();
+
+        int[] original_array = new int[size];
+
+        System.out.print("Enter array elements : ");
+        for (int i = 0; i < original_array.length; i++){
+            original_array[i] = sc.nextInt();
+        }
+        System.out.println("Original Array : ");
+        printArray(original_array);
+
+
+//        array cloning
+
+        int[] cloneArray = original_array;
+
+        System.out.println("Clone Array : ");
+        printArray(cloneArray);
+    }
+}
