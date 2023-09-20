10eebc87ab56cd4cbc5a70673f9ecc78bb9188fb
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-09-20 13:59:36 +0530

    arrayInput program file added

---
+++ src/Arrays/arrayInput.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+import java.util.Scanner;
+public class arrayInput {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of an Array : ");
+        int size = sc.nextInt();
+        int [] arr= new int[size];
+
+        System.out.println("Enter array elements : ");
+        for (int i = 0; i< arr.length; i++){
+            arr[i] = Integer.parseInt(sc.next());
+        }
+
+
+        System.out.println("Elements of array : ");
+        for (int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+
+}
