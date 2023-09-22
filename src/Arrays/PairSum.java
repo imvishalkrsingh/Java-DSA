26196de3260e8fa8fbcf2a5fc50151afc9da8070
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-09-22 18:15:43 +0530

    pairSum program file added

---
+++ src/Arrays/PairSum.java
@@ -0,0 +1,24 @@
+package Arrays;
+import java.util.Scanner;
+public class PairSum {
+
+    static int pairSum(int[] arr, int target){
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of an Array : ");
+        int size = sc.nextInt();
+
+        int[] array = new int[size];
+
+        System.out.println("Enter elements of an array : ");
+        for (int i = 0; i<= array.length; i++){
+            array[i] = sc.nextInt();
+        }
+
+
+    }
+}
