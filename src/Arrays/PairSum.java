df714cf9079528e31700e36be4a4e9374174b909
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-09-22 18:27:59 +0530

    PairSum program updated

--- src/Arrays/PairSum.java
+++ src/Arrays/PairSum.java
@@ -3,6 +3,17 @@ import java.util.Scanner;
 public class PairSum {
 
     static int pairSum(int[] arr, int target){
+        int n = arr.length;
+        int ans = 0;
+
+        for (int i = 0; i<n; i++){
+            for (int j = i+1; j<n; j++){
+                if(arr[i] + arr[j] == target){
+                    ans++;
+                }
+            }
+        }
+        return ans;
 
     }
 
@@ -15,10 +26,13 @@ public class PairSum {
         int[] array = new int[size];
 
         System.out.println("Enter elements of an array : ");
-        for (int i = 0; i<= array.length; i++){
+        for (int i = 0; i < array.length; i++){
             array[i] = sc.nextInt();
         }
 
+        System.out.print("Enter Target Value : ");
+        int target = sc.nextInt();
 
+        System.out.println("Pair sum of " + target + " : " + pairSum(array,target));
     }
 }
