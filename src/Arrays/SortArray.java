7e75a1ceb3a499d3bf4b475a591c52aa91d4f348
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-09-25 09:54:45 +0530

    sortZeroesAndOnes method added

--- src/Arrays/SortArray.java
+++ src/Arrays/SortArray.java
@@ -2,6 +2,8 @@ package Arrays;
 import java.util.Scanner;
 public class SortArray {
 
+    // Q. Sort an array consisting only 0s and 1s
+
     static void printArray(int[] arr){ // method to print array
         int n = arr.length;    // size of an array
         for(int i = 0; i < n; i++){
@@ -15,6 +17,29 @@ public class SortArray {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
+    }
+
+    static void sortZeroesAndOnes(int[] arr){
+        int n = arr.length;
+        int zeroes = 0;
+
+        // Count number of zeroes
+        for (int i = 0; i < n; i++){
+            if(arr[i] == 0){
+                zeroes++;
+            }
+        }
+
+        //print 0 to zeroes -1 : 0 and zeroes to n-1 : 1
+
+        for (int i = 0; i < n; i++){
+            if(i < zeroes){
+                arr[i] = 0;
+            }
+            else {
+                arr[i] = 1;
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -31,6 +56,11 @@ public class SortArray {
         }
 
         System.out.println("Here Is the Original Array : ");
+        printArray(array);
+
+        sortZeroesAndOnes(array);
+
+        System.out.println("Here is the Sorted Array : ");
         printArray(array);
 
     }
