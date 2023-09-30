d3e8c7e0204a551bd63160fa43e01c28a8cd2f3e
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-09-30 19:56:27 +0530

    Half_pyramid of number program file added

---
+++ src/Loops/Pattern_Printing/Half_Pyramid.java
@@ -0,0 +1,18 @@
+package Loops.Pattern_Printing;
+import java.util.Scanner;
+public class Half_Pyramid {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number of rows : ");
+        int rows = sc.nextInt();
+
+        for (int i = 0; i <= rows; i++){
+            for (int j = 1; j <= i; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
