8120d8f2d3598f3a62080ccff08084e59b2ef895
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-10-02 08:41:45 +0530

    Reverse of the array list program file added

---
+++ src/Arrays/Arraylist/ReverseArrayList.java
@@ -0,0 +1,41 @@
+/*
+Q. Write a program to Reverse the given Array List
+ */
+
+package Arrays.Arraylist;
+
+import java.util.ArrayList;
+
+public class ReverseArrayList {
+
+    static  void reverseList(ArrayList<Integer> list){
+        int i = 0, j = list.size() -1;
+
+        while (i < j){
+            Integer temp = Integer.valueOf(list.get(i));
+            list.set(i, list.get(j));
+            list.set(j, temp);
+
+            i++;
+            j--;
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(0);
+        list.add(10);
+        list.add(3);
+        list.add(5);
+        list.add(22);
+        list.add(10);
+
+        System.out.println("Original List : " + list);
+        reverseList(list);
+
+        System.out.print("Reversed list : ");
+        System.out.println("Reversed List " + list);
+
+    }
+}
