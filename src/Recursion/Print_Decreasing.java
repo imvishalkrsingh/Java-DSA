dae31bf9646d53f76f31b4fe78246925d8a89e17
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-10-04 17:24:11 +0530

    Print_Decreasing program file added

---
+++ src/Recursion/Print_Decreasing.java
@@ -0,0 +1,29 @@
+// Q. Write a program to print all natural numbers from n to 1 using recursion.
+package Recursion;
+import java.util.Scanner;
+
+public class Print__N_to_1 {
+
+    static void printDecreasing(int n){
+        if (n == 1){
+            System.out.println(1);
+            return;
+        }
+//        self Work
+        System.out.println(n);
+
+//        recursive work
+        printDecreasing(n-1);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter value of N : ");
+        int n = sc.nextInt();
+
+        printDecreasing(n);
+
+    }
+}
