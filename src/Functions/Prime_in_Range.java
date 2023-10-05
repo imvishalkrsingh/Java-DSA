4d355ebd2a935076ba26f57be9c60d1007e8e65a
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-10-05 17:08:21 +0530

    Prime_in_Range program file added

--- src/Functions/Prime_in_Range.java
+++ src/Functions/Prime_in_Range.java
@@ -1,2 +1,31 @@
-package Functions;public class Prime_in_Range {
+package Functions;
+import java.util.Scanner;
+public class Prime_in_Range {
+
+    static boolean isPrime(int n){
+        for (int i = 2; i <= Math.sqrt(n); i++){
+            if (n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    static void primesRange(int n){
+        for (int i = 2; i <= n; i++){
+            if (isPrime(i) == true ){
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Range to print prime Number : ");
+        int number = sc.nextInt();
+
+        System.out.println("Prime numbers between 2 and " + number + " : ");
+        primesRange(number);
+    }
 }
