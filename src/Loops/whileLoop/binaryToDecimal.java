b0ce7717ebd6e2f6ec97fb4e58372ce81193dcef
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-10-05 21:42:04 +0530

    another method added

--- src/Loops/whileLoop/binaryToDecimal.java
+++ src/Loops/whileLoop/binaryToDecimal.java
@@ -7,15 +7,29 @@ public class binaryToDecimal {
         System.out.print("Enter binary Number : ");
         int binary_num = sc.nextInt();
 
-        int ans = 0; // Converted decimal number
-        int power = 1; // 2 ^ 0 = 1 power of 2
+//        int ans = 0;         // Converted decimal number
+//        int power = 1;       // 2 ^ 0 = 1 power of 2
 
-        while(binary_num > 0){
-            int unit_digit = binary_num % 10;
-            ans = ans + (unit_digit * power);
+//        while(binary_num > 0){
+//            int unit_digit = binary_num % 10;
+//            ans = ans + (unit_digit * power);
+//            binary_num = binary_num / 10;
+//            power = power * 2;
+//        }
+//        System.out.println("Decimal Number : " + ans);
+
+
+//        +++++++++++++++++++Another Method+++++++++++++++++++++
+        int ans1 = 0;
+        int power1 = 0;
+        while (binary_num > 0){
+
+            int unitDigit = binary_num % 10;
+            ans1 = ans1 + (unitDigit * (int)Math.pow(2, power1));
+
+            power1++;
             binary_num = binary_num / 10;
-            power = power * 2;
         }
-        System.out.println("Decimal Number : " + ans);
+        System.out.println("Decimal Number : " + ans1);
     }
 }
