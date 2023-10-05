64ec1034de203154abd47d188dfc1132e1bc0915
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-10-05 23:43:57 +0530

    inverted_half_pyramid_with_number method added

--- src/Functions/Pyramid_Star.java
+++ src/Functions/Pyramid_Star.java
@@ -19,12 +19,23 @@ public class Pyramid_Star {
         }
     }
 
+    static void inverted_half_pyramid_with_number(int n){
+        for (int i = 1; i <= n; i++ ){
+            for (int j = 1; j <= n-i+1; j++ ){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter number of rows: ");
         int rows = sc.nextInt();
 
-        inverted_rotated_half_pyramid(rows);
+//        inverted_rotated_half_pyramid(rows);
+
+        inverted_half_pyramid_with_number(rows);
     }
 }
