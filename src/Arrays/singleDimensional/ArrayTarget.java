bc0811d95234f9117b65a6bac444945b53bfb735
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-10-08 13:09:22 +0530

    ArrayTarget file added

---
+++ src/Arrays/singleDimensional/ArrayTarget.java
@@ -0,0 +1,23 @@
+package Arrays.singleDimensional;
+import java.util.Scanner;
+public class ArrayTarget {
+
+        public static void main(String[] args) throws Exception {
+            Scanner scn = new Scanner(System.in);
+            int n = scn.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = scn.nextInt();
+            }
+            int target = scn.nextInt();
+            for (int i = 0; i < arr.length; i++) {
+                if (target == arr[i]) {
+                    System.out.println(i);
+                    return;
+                }
+            }
+            System.out.println(-1);
+        }
+    }
+
+
