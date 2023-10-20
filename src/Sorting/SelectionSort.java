50f26f140abd4c338675bb55aaccecc4c6ef3a5f
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-10-20 08:35:59 +0530

    Selection sort program file added

--- src/Sorting/SelectionSort.java
+++ src/Sorting/SelectionSort.java
@@ -12,7 +12,7 @@ public class SelectionSort {
     public static void selectionSort(int[] arr){
         for (int i = 0; i < arr.length-1; i++){
             int minPosition = i;
-            for (int j = i+1; j < arr.length-1; j++){
+            for (int j = i+1; j < arr.length; j++){
                 if(arr[minPosition] > arr[j]){
                     minPosition = j;
                 }
