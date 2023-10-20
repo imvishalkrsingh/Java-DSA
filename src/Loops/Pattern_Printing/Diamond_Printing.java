03b4330051faeb2412720ce0e17897e9808d5fd6
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-10-20 07:50:01 +0530

    Updated

--- src/Loops/Pattern_Printing/Diamond_Printing.java
+++ src/Loops/Pattern_Printing/Diamond_Printing.java
@@ -11,19 +11,19 @@ public class Diamond_Printing {
 
         for (int i = 1; i <= rows; i++){
             for (int j = 1; j <= rows - i; j++){
-                System.out.print(" ");
+                System.out.print("   ");
             }
             for (int j = 1; j <= 2*(i-1)+1; j++){
-                System.out.print("*");
+                System.out.print(" * ");
             }
             System.out.println();
         }
         for (int i = rows; i >=1; i--){
             for (int j = 1; j <= rows - i; j++){
-                System.out.print(" ");
+                System.out.print("   ");
             }
             for (int j = 1; j <= 2*(i-1)+1; j++){
-                System.out.print("*");
+                System.out.print(" * ");
             }
             System.out.println();
         }
