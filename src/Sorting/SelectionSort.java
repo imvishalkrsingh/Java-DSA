c317b52f297b40a4c59f80808d70cb7b735fe7c2
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-10-20 08:34:21 +0530

    Selection sort program file added

---
+++ src/Sorting/SelectionSort.java
@@ -0,0 +1,46 @@
+package Sorting;
+import java.util.Scanner;
+public class SelectionSort {
+
+    public static void printArray(int[] arr){
+        for (int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void selectionSort(int[] arr){
+        for (int i = 0; i < arr.length-1; i++){
+            int minPosition = i;
+            for (int j = i+1; j < arr.length-1; j++){
+                if(arr[minPosition] > arr[j]){
+                    minPosition = j;
+                }
+            }
+            // Swap
+            int temp = arr[minPosition];
+            arr[minPosition] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of Array : ");
+        int size = sc.nextInt();
+
+        int[] array = new int[size];
+
+        System.out.println("Enter " + size + " Elements of Array : ");
+        for (int i = 0; i < size; i++){
+            array[i] = sc.nextInt();
+        }
+
+        System.out.println("Here is the Original Array : ");
+        printArray(array);
+
+        System.out.println("Here is Sorted Array by Selection Sort : ");
+        selectionSort(array);
+        printArray(array);
+    }
+}
