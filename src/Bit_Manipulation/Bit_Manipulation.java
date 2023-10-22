d9926458faed9e2ef9ca2969c5171816a2368ce8
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-10-22 10:09:58 +0530

    Bit manipulation program file added

---
+++ src/Bit_Manipulation/Bit_Manipulation.java
@@ -0,0 +1,30 @@
+package Bit_Manipulation;
+
+public class Bit_Manipulation {
+    public static void main(String[] args) {
+//        Binary AND
+        System.out.print("Binary AND : ");
+        System.out.println(5 & 6);
+
+//        Binary OR
+        System.out.print("Binary OR : ");
+        System.out.println(5 | 6);
+
+//        Binary XOR
+        System.out.print("Binary XOR : ");
+        System.out.println(5 ^ 6);
+
+//        Binary 1's Compliment
+        System.out.print("Binary 1's Compliment : ");
+        System.out.println(~5);
+
+//        Binary Left shift
+        System.out.print("Binary Left shift : ");
+        System.out.println(5 << 6);
+
+//        Binary right shift
+        System.out.print("Binary Right shift : ");
+        System.out.println(6 >> 1);
+
+    }
+}
