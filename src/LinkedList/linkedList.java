57316139aeb8baa83b4e076095229162acedbbc5
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-11-10 20:03:50 +0530

    added remove first method

--- src/LinkedList/linkedList.java
+++ src/LinkedList/linkedList.java
@@ -14,9 +14,12 @@ public class linkedList{
     public static Node head;
     public static Node tail;
 
+    public static int size;
+
     public void addFirst(int data){
         // step 1 : Create new node
         Node newNode = new Node(data);
+        size++;
         if(head == null){
             head = tail = newNode;
             return;
@@ -32,6 +35,7 @@ public class linkedList{
     public void addLast(int data){
         // step 1 :
         Node newNode = new Node(data);
+        size++;
         if(head == null){
             head = tail = newNode;
             return;
@@ -63,6 +67,7 @@ public class linkedList{
     // add in the middle
     public void addMid(int idx, int data){
         Node newNode = new Node(data);
+        size++;
         Node temp = head;
         int i = 0;
 
@@ -76,6 +81,21 @@ public class linkedList{
         temp.next = newNode;
     }
 
+    // Remove first
+    public int removeFirst(){
+        if(size == 0){
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        return val;
+    }
+
     public static void main(String[] args) {
         linkedList l1 = new linkedList();
         l1.addFirst(1);
@@ -85,6 +105,10 @@ public class linkedList{
         l1.addLast(4);
         l1.addMid(4,9);
 
+        l1.printLL();
+        System.out.println("size of Linked List : " + l1.size);
+
+        l1.removeFirst();
         l1.printLL();
     }
 }
