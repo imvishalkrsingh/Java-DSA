8390ee4855142a0b95b33702ff2201c9d6b316a2
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-11-14 08:49:46 +0530

    This keyword program program file added

--- src/Oops/Keywords/ThisKeyword.java
+++ src/Oops/Keywords/ThisKeyword.java
@@ -1,2 +1,24 @@
-package Oops.Keywords;public class ThisKeyword {
+package Oops.Keywords;
+
+public class ThisKeyword {
+
+    public class Person {
+        String name;
+        int age;
+        String gender;
+
+        public Person(String name, int age, String gender) {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+        }
+    }
+
+    public static void main(String[] args) {
+        ThisKeyword.Person p1 = new ThisKeyword().new Person("Vishal", 24, "Male");
+
+        System.out.println(p1.name);
+        System.out.println(p1.age);
+        System.out.println(p1.gender);
+    }
 }
