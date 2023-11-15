ffb32013569681f41524b6cd47d2dc7272923582
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-11-15 09:00:08 +0530

    Basic Linkedlist operation

---
+++ src/LinkedList/BasicLinkedList.java
@@ -0,0 +1,15 @@
+package LinkedList;
+
+public class BasicLinkedList {
+
+    public static class Node {
+        int data;
+    }
+
+    public static void main(String[] args) {
+
+        Node x = new Node();
+        System.out.println(x.data);;
+
+    }
+}
