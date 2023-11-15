be020dc133de3654c49a684797376239fa1e0570
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-11-15 09:02:24 +0530

    Basic linked list operation

--- src/LinkedList/BasicLinkedList.java
+++ src/LinkedList/BasicLinkedList.java
@@ -4,12 +4,18 @@ public class BasicLinkedList {
 
     public static class Node {
         int data;
+        Node next;
+
+        // Constructor
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
     }
 
     public static void main(String[] args) {
-
-        Node x = new Node();
-        System.out.println(x.data);;
-
+        // Creating a new node with data 42
+        Node x = new Node(42);
+        System.out.println(x.data);
     }
 }
