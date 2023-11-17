2dcbded29a4da6b8dafe07bae5782e07cf2b87c1
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-11-17 10:59:14 +0530

    Added more code

--- src/LinkedList/BasicLinkedList.java
+++ src/LinkedList/BasicLinkedList.java
@@ -16,6 +16,20 @@ public class BasicLinkedList {
     public static void main(String[] args) {
         // Creating a new node with data 42
         Node x = new Node(42);
-        System.out.println(x.data);
+        System.out.println("Data of x : " + x.data);
+
+
+        Node a = new Node(5);
+        Node b = new Node(3);
+        Node c = new Node(9);
+        Node d = new Node(8);
+        Node e = new Node(16);
+
+        // 5 -> 3 -> 9 -> 8 -> 16
+        a.next = b;
+        System.out.println("Data of a : " + a.data);
+        System.out.println(a.next);
+        System.out.println("Data of b : " + a.next.data);
+
     }
 }
