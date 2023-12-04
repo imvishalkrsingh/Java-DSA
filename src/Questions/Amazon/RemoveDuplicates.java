47012aa9cd8d6f0ce4e41a351465e7b420dafeec
imvishalkrsingh <vishalkumarsingh162@gmail.com>
2023-12-04 17:07:11 +0530

    Created a separate package for placement questions

---
+++ src/Questions/Amazon/RemoveDuplicates.java
@@ -0,0 +1,21 @@
+package Questions.Amazon;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        int nums[] = new int[7];
+        nums[0] = 0;
+        nums[1] = 1;
+        nums[2] = 1;
+        nums[3] = 1;
+        nums[4] = 2;
+        nums[5] = 2;
+        nums[6] = 3;
+
+        System.out.println(mergeSortedArray(nums));
+    }
+
+    public static int mergeSortedArray(int[] nums){
+        int i = 0;
+        int j = 1;
+    }
+}
